1bdfc84cad678494c50b127c6bee2adbb11c1289
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-09-30 16:23:57 +0100

    register user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using API.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using API.Entities;
 
 namespace API.Controllers
 {
@@ -18,6 +23,19 @@ namespace API.Controllers
         public async Task<ActionResult<AppUser>> Register(string username, string password)
         {
             using var hmac = new HMACSHA512();
+
+            var user = new AppUser()
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.users.Add(user);
+
+            await _context.SaveChangesAsync();
+
+            return user;
         }
     }
 }
