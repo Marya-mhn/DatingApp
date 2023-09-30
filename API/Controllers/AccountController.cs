e441b8a02e3e320e32e6363c6a0d154dd9b44670
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-09-30 15:58:59 +0100

    commit

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
-
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
     }
 }
