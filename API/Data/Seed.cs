45f26c1cbdb09854aa7c3485c1c404b59b0bd10c
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-10-26 18:19:23 +0100

    seeding data

---
+++ API/Data/Seed.cs
@@ -0,0 +1,33 @@
+using API.Entities;
+using System.Linq;
+using System.Text.Json;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context){
+            if(await context.users.AnyAsync()) return;
+
+            var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+
+            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+            foreach(var user in users){
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                user.PasswordSalt = hmac.Key;
+
+                context.users.Add(user);
+            }
+            await context.SaveChangesAsync();
+        }
+    }
+} 
