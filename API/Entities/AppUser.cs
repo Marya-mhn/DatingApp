93a2b0fde2dde4164898d572aff62c91322ef83e
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-10-26 16:52:43 +0100

    adding photos table

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;  
 
 namespace API.Entities
 {
@@ -11,5 +12,21 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos { get; set; } = new();
+
+        public int GetAge()
+        { 
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
