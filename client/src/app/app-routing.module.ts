8c1aa605273fb0e07cb58948e2cbe646aa27103b
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-10-27 19:36:07 +0100

    adding the member detail page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
         path: 'members',
         component: MemberListComponent,
       },
-      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'members/:username', component: MemberDetailComponent },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ],
