fdbc1d90efa2a4f7412505cf6293e61f8fcee890
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-10-27 14:38:19 +0100

    adding member cards

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
+  styleUrls: ['./member-card.component.css'],
 })
-export class MemberCardComponent {
+export class MemberCardComponent implements OnInit {
+  @Input() member: Member | undefined;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
