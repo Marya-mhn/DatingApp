1ee13c546f5b90d25fe5ad83d8ce974b9127e20b
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-11-05 08:49:25 +0100

    adding an endpoint for adding a photo

---
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace API.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
