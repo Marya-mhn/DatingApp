609e4f885364b8c26f1446abee6b161085a6119f
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-11-28 20:48:50 +0100

    making last active more optimal

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -10,6 +10,11 @@ namespace API.Extensions
     {
         public static string GetUsername(this ClaimsPrincipal user)
         {
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
             return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         }
     }
--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -17,10 +17,10 @@ namespace API.Helpers
 
             if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
 
-            var username = resultContext.HttpContext.User.GetUsername();
+            var userId = resultContext.HttpContext.User.GetUserId();
 
             var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
-            var user = await repo.GetUserByUsernameAsync(username);
+            var user = await repo.GetUserByIdAsync(int.Parse(userId));
             user.LastActive = DateTime.UtcNow;
             await repo.SaveAllAsync();
         }
