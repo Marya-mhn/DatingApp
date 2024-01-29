c639bf9567ebed49f62d91691a745defcfe5b7a5
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2024-01-29 20:22:45 +0100

    adding the message thread in the client side

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -21,4 +21,10 @@ export class MessageService {
       this.http
     );
   }
+
+  getMessageThread(username: string) {
+    return this.http.get<Message[]>(
+      this.baseUrl + 'messages/thread/' + username
+    );
+  }
 }
