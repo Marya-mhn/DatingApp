ff211286900da66fc6bf38a5c90cecf3f32899ad
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2024-02-18 20:03:10 +0100

    Identity and role management; setting up the entities

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -9,25 +9,23 @@ namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(DataContext context){
-            if(await context.users.AnyAsync()) return;
+        public static async Task SeedUsers(DataContext context)
+        {
+            if (await context.users.AnyAsync()) return;
 
             var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
 
-            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
 
-            foreach(var user in users){
-                using var hmac = new HMACSHA512();
-
+            foreach (var user in users)
+            {
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
-                user.PasswordSalt = hmac.Key;
 
                 context.users.Add(user);
             }
             await context.SaveChangesAsync();
         }
     }
-} 
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,15 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Extensions;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities
 {
-    public class AppUser
+    public class AppUser : IdentityUser<int>
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
         public DateOnly DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
@@ -27,5 +24,6 @@ namespace API.Entities
         public List<UserLike> LikedUsers { get; set; }
         public List<Message> MessagesSent { get; set; }
         public List<Message> MessagesReceived { get; set; }
+        public ICollection<AppUserRole> UserRoles { get; set; }
     }
 }
