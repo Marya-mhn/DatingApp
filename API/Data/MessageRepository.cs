9a8fcaa575f1ce68a9536e72216a99d1b5a95fe4
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2024-02-18 15:08:19 +0100

    Deleting messages on the client

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -51,9 +51,12 @@ namespace API.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username),
-                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username),
-                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.DateRead == null),
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username
+                    && u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username
+                    && u.SenderDeleted == false),
+                _ => query.Where(u => u.RecipientUsername == messageParams.Username
+                    && u.RecipientDeleted == false && u.DateRead == null),
 
             };
 
@@ -67,9 +70,9 @@ namespace API.Data
                 .Include(u => u.Sender).ThenInclude(p => p.Photos)
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(
-                    m => (m.RecipientUsername == currentUserName &&
+                    m => (m.RecipientUsername == currentUserName && m.RecipientDeleted == false &&
                     m.SenderUsername == recipientUserName) ||
-                    (m.RecipientUsername == recipientUserName &&
+                    (m.RecipientUsername == recipientUserName && m.SenderDeleted == false &&
                     m.SenderUsername == currentUserName)
                 )
                 .OrderBy(m => m.MessageSent)
